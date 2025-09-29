1bdf260cbfb486156cc40e4032feb316d636df2b
MD. Kajam-all Koraish <koraishmdkajamall38@gmail.com>
2025-09-29 13:31:36 +0600

    Add files via upload

---
+++ _00_Problem_solving/_03_Area _of_a_Circle.kt
@@ -0,0 +1,12 @@
+package _00_Problem_solving
+
+import java.util.*
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val R = scanner.nextDouble()
+    val pi = 3.14159
+
+    val a = pi * R * R
+    println("A=%.4f".format(a))
+}
