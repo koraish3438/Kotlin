05b14dec13eff0a8b3343a52ba0f9b7597a87e0a
MD. Kajam-all Koraish <koraishmdkajamall38@gmail.com>
2025-09-29 10:39:03 +0600

    Delete 02_controlFlow directory

--- 02_controlFlow/src/_05_ForLoop/syntax.kt
+++
@@ -1,9 +0,0 @@
-package _02_controlFlow._05_ForLoop
-
-fun main() {
-    //======Syntax===
-//    for (item in collection) {
-//        // Code to be executed for each item
-//    }
-
-}
--- 02_controlFlow/src/_06_doWhileLoop/syntax.kt
+++
@@ -1,8 +0,0 @@
-package _02_controlFlow._06_doWhileLoop
-
-fun main() {
-//    do {
-//        // code block to be executed
-//    }
-//    while (condition);
-}
