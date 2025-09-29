d13dcf261041d50efcded01c5363569906f10a62
MD. Kajam-all Koraish <koraishmdkajamall38@gmail.com>
2025-09-29 10:38:45 +0600

    Delete 01_basics/KotlinBasic directory

--- 01_basics/KotlinBasic/src/_2_comment.kt
+++
@@ -1,5 +0,0 @@
-fun main() {
-    println("Single-line comments starts with two forward slashes (//)") //this is single line comments
-
-    println("Multi-line comments start with /* and ends with */") //Any text between /* and */ will be ignored by Kotlin.
-}
