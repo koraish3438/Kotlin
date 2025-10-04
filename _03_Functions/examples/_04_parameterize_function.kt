e5bf221a7601500128c548fc01b659493040a00f
MD. Kajam-all Koraish <koraishmdkajamall38@gmail.com>
2025-10-04 14:48:27 +0600

    Add files via upload

---
+++ _03_Functions/examples/_04_parameterize_function.kt
@@ -0,0 +1,28 @@
+package _03_Functions.examples
+
+fun sum (a: Int, b: Int) {
+    val sum = a + b
+    println("Addition is : $sum")
+}
+
+fun sub (a: Int, b: Int) {
+    val sub = a - b
+    println("Subtraction is : $sub")
+}
+
+fun mul (a: Int, b: Int) {
+    val mul = a * b
+    println("Multiplication is : $mul")
+}
+
+fun divi (a: Int, b: Int) {
+    val divi = a / b
+    println("Division is : $divi")
+}
+
+fun main() {
+    sum(5,8)
+    sub(15,8)
+    mul(8,8)
+    divi(64,8)
+}
